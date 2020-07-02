dd4c2461e2dd9e7b6acde4f9ddcad5341314dd99
ovinafas <ovinafas@gmail.com>
2020-07-03 00:17:20 +0300

    second commit

---
+++ template/app/show-article.php
@@ -0,0 +1,79 @@
+<?php 
+    session_start();
+    require_once(realpath(dirname(__FILE__) . "/../app/layouts/head-tag.php"));
+
+?>
+
+
+
+    <section class="content">
+
+        <section class="container">
+            <main class="main">
+                <section class="main-news">
+                    <h2 class="title"><?php echo $article['title'];   ?></h2>
+                    <article>
+
+                        <img class="main-news-img" src="http://localhost/admin-panel/<?php echo $article['image']; ?>" alt="">
+                        <h3 class="article-title">
+                            <a href="http://localhost/admin-panel/show-article/1"><?php echo  $article['title'];   ?></a>
+                        </h3>
+                        <ul class="info-bar">
+                            <li class=""><span class="text-muted">by</span> <a href="#" class="color-black"><b><?php  echo $username['username'];  ?></b></a>
+                                <span class="text-muted"><?php echo  date("M d,Y",strtotime($article['created_at']));  ?></span></li>
+                            <li><i class="fas fa-bolt text-yellow"></i> <?php echo $article['view'];   ?></li>
+                            <li><i class="fas fa-comments text-yellow"></i> <?php echo      $commentsCount['COUNT(*)']; ?></li>
+                        </ul>
+                        <p><?php $article['summary']; ?></p>
+
+                        <p class="footer-p-margin-20"><?php $article['body'];  ?></p>
+
+                    </article>
+                      <?php  foreach($comments as $comment) {  ?>
+                        <section class="comment-box">
+                            <h3 class="comment-box-header">
+
+                            <?php   $comment['username'];  ?>
+                                <span class="comment-box-date"><?php echo  date("M d,Y",strtotime($comment['created_at']));  ?></span>
+                            </h3>
+                            <comment>
+                               <?php   $comment['comment'];  ?>
+                            </comment>
+                        </section>
+
+                      <?php }  ?>
+
+                      <?php //if(isset($_SESSION['user'])) { ?> 
+
+                    <form action="http://localhost/admin-panel/comment-store" method="post">
+                            <input name="article_id" type="hidden" value="1">
+                        <textarea class="comment" name="comment" rows="4" required placeholder="your comment ..."></textarea>
+                        <input class="submit" type="submit" value="store comment">
+                    </form>
+
+                    <?php // }    ?>
+
+                </section><!--end of main news-->
+
+
+            </main><!--end of main-->
+
+
+    
+                    
+
+<?php     
+       require_once(realpath(dirname(__FILE__) . "/../app/layouts/sidebar.php")); 
+?>
+
+
+            <section class="clear-fix"></section>
+        </section><!--end of container-->
+    </section><!--end of content-->
+    </section><!--end of first app section-->
+
+
+<?php
+
+require_once(realpath(dirname(__FILE__) . "/../app/layouts/footer.php"));
+?>
---
+++ template/app/show-category.php
@@ -0,0 +1,55 @@
+<?php
+             //session_start();
+             require_once(realpath(dirname(__FILE__) . "/../app/layouts/head-tag.php"));
+               
+?>
+    <section class="content">
+        <section class="container">
+            <main class="main">
+                <section class="main-crypto-mining-news">
+
+                    <h2 class="title"><?php  echo  $category['name'];  ?></h2>
+
+                     <?php foreach($articles  as $article) { ?>
+
+                    <section class="main-news-w-50">
+                        <article>
+                            <img class="main-news-img" src="http://localhost/admin-panel/<?php  $article['image'];  ?>" alt="">
+                            <h3 class="article-title">
+                                <a href="http://localhost/admin-panel/show-article/<?php echo  $article['id'];   ?>"><?php echo $article['title'];    ?></a>
+                            </h3>
+                            <ul class="info-bar">
+                                <li class=""><span class="text-muted">by</span> <a href="#" class="color-black"><b><?php  echo  $article['username'] ;  ?></b></a>
+                                    <span class="text-muted"><?php echo  date("M d,Y",strtotime($article['created_at']));  ?></span></li>
+                                <li><i class="fas fa-bolt text-yellow"></i><?php  echo $article['view']; ?></li>
+                                <li><i class="fas fa-comments text-yellow"></i> <?php  echo  $article['comments_count']   ?></li>
+                            </ul>
+                        </article>
+                    </section>
+
+                     <?php   }   ?>
+
+                    <section class="clear-fix"></section>
+                </section><!--end of main crypto mining news-->
+            </main><!--end of main-->
+
+          
+
+
+<?php
+          require_once(realpath(dirname(__FILE__) . "/../app/layouts/sidebar.php")); 
+
+?>
+
+
+            <section class="clear-fix"></section>
+        </section><!--end of container-->
+    </section><!--end of content-->
+    </section><!--end of first app section-->
+
+
+
+<?php
+
+         require_once(realpath(dirname(__FILE__) . "/../app/layouts/footer.php"));
+?>
